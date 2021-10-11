b601028d2b72324a84c7200920d03c13bb0fd887
murasaki21 <47200816+murasaki21@users.noreply.github.com>
2021-10-10 20:30:19 -0700

    Update ex3.js

---
+++ ex3.js
@@ -0,0 +1,16 @@
+let gridSize = 8;
+
+let chessboard = "";
+
+for (let y = 0; y < gridSize; y++) {
+  for (let x = 0; x < gridSize; x++) {
+    if ((x + y) % 2 == 0) {
+      chessboard += " ";
+    } else {
+      chessboard += "#";
+    }
+  }
+  chessboard += "\n";
+}
+
+console.log(chessboard);
