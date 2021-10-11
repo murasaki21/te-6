9e76eba471a7caff7635078ceadf8739663f7be8
murasaki21 <47200816+murasaki21@users.noreply.github.com>
2021-10-10 20:34:28 -0700

    add comment to ex3

--- ex3.js
+++ ex3.js
@@ -4,6 +4,7 @@ let chessboard = "";
 
 for (let y = 0; y < gridSize; y++) {
   for (let x = 0; x < gridSize; x++) {
+    //creates checkered pattern
     if ((x + y) % 2 == 0) {
       chessboard += " ";
     } else {
